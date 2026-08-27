9e385378c824717c009934bad90edd46c8c58ad6
shiota-2021 <shiota-satoshi@ncontr.com>
2026-08-27 18:11:00 +0900

    exastro-it-automation#3075 webページ資材バージョンを2.10.0に変更

--- ita_root/ita_web_server/contents/common/js/common.js
+++ ita_root/ita_web_server/contents/common/js/common.js
@@ -29,7 +29,7 @@ const fn = ( function() {
     'use strict';
 
     // バージョン
-    const version = '2.9.0';
+    const version = '2.10.0';
 
     // AbortController
     const controller = new AbortController();
