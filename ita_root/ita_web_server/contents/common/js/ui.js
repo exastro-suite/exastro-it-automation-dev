a5992264c76d31007072240ed60fd844b1944e61
katokeita <katokeita@ncontr.com>
2026-09-18 16:06:37 +0900

    #3079 画面修正

--- ita_root/ita_web_server/contents/common/js/ui.js
+++ ita_root/ita_web_server/contents/common/js/ui.js
@@ -1958,6 +1958,8 @@ aiAssistant() {
         { type: 'js', url: '/_/ita/js/ai_assistant.js'},
         { type: 'js', url: '/_/ita/js/ai_assistant/ai_assistant_setting.js'},
         { type: 'js', url: '/_/ita/js/ai_assistant/ai_assistant_llm.js'},
+        // 会話履歴・学習事項のインポート／エクスポート
+        { type: 'js', url: '/_/ita/js/ai_assistant/ai_assistant_transfer.js'},
         // 画面専用ツール（ツールごとに定義と動作を1ファイルにまとめている）
         { type: 'js', url: '/_/ita/js/ai_assistant/tools/ai_assistant_tool_ask_user_choice.js'},
         { type: 'js', url: '/_/ita/js/ai_assistant/tools/ai_assistant_tool_display_html.js'},
